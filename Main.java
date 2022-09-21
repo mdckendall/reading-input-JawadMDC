f7660ac69955492c482bfe655b3336882605110c
Jawad Mohammed <113356691+JawadMDC@users.noreply.github.com>
2022-09-21 08:48:59 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     Scanner newScanner = new Scanner(System.in);
     System.out.println("Enter a word:");
-    String newString = myScanner.nextLine();
+    String newString = newScanner.nextLine();
     
     //Your code will return the length of the entered String
     newScanner.close();
